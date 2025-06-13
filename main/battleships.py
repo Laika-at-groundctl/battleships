3f19b80a221040a349e6368386070319da79e9b2
Lakia-at-groundctl <alexi.bean@students.whs.co.nz>
2025-06-13 12:17:28 +1200

    started working on user input

--- main/battleships.py
+++ main/battleships.py
@@ -19,6 +19,9 @@ BgColour = "#1e1e2e"
 TxtColour = "#cdd6f4"
 BgColour1 = "#9399b2"
 
+# font
+MainFont = ["JetBrains Mono", "13"]
+
 # for debugging
 print(position1x, position1y, position2x, position2y, position3x, position3x)
 
@@ -32,14 +35,36 @@ root.configure(bg=BgColour, border=5)
 root.title("BATTLESHIPS")
 
 # set window size
-root.geometry("600x600")
+root.geometry("700x700")
 
 # create grid
 for row in range(10):
     for col in range(10):
-        btn = Button(root, bg=BgColour1, fg=TxtColour, font=("JetBrains Mono", "13"),
+        btn = Button(root, bg=BgColour1, fg=TxtColour, font=MainFont,
                      text=f"{row},{col}", width=1, height=1)
         btn.grid(row=row, column=col, padx=0, pady=0)
 
+# string variables
+X_var = StringVar()
+
+Y_var = StringVar()
+
+# player inputs
+Entry1Label = Label(root, text="enter x coordinate",
+                    font=MainFont, bg=BgColour1, fg=TxtColour,)
+
+Entry2Label = Label(root, text="enter y coordinate",
+                    font=MainFont, bg=BgColour1, fg=TxtColour)
+
+Entry1 = Entry(root, textvariable=X_var, font=MainFont,
+               bg=BgColour1, fg=TxtColour)
+
+Entry2 = Entry(root, textvariable=Y_var, font=MainFont,
+               bg=BgColour1, fg=TxtColour)
+
+Entry1Label.grid(row=10, column=10)
+Entry2Label.grid(row=11, column=10)
+Entry1.grid(row=10, column=11)
+Entry2.grid(row=11, column=11)
 # Window end
 root.mainloop()
